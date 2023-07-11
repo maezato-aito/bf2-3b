1658aecaa4437ca87d219b728610cf844c43024f
Your Name <aito-maezato-k22f@stu.kbc.ac.jp>
2023-07-11 13:08:01 +0900

    プレイヤー　操作

--- bf2-3b/Player.cpp
+++ bf2-3b/Player.cpp
@@ -1,9 +1,11 @@
 #include"DxLib.h"
 #include"Player.h"
-
-
+#include"Stage.h"
+#include"common.h"
 Player::Player()
 {
+
+	//プレイヤー初期位置
 	boxX=100;
 	boxX2=150;
 	boxY=100;
@@ -16,19 +18,36 @@ Player::~Player()
 }
 AbstractScene* Player::Update()
 {
+	//アナログスティックの傾き
 	GetJoypadAnalogInput(&InputX, &InputY, DX_INPUT_PAD1);
+
+	//
+	if ((GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_2) == 0 )
+	{
+		if (S1_Landright_Y > boxY2) {
+			boxY += 4;
+			boxY2 += 4;
+		}
+	}
+	else
+	{
+		if(boxY>0)
+		boxY -= 7;
+		boxY2 -= 7;
+	}
+	//画面内に描画
 	if (boxX > 0) {
 		if (InputX < -1)
 		{
-			boxX -= 10;
-			boxX2 -= 10;
+			boxX -= 5;
+			boxX2 -= 5;
 		}
 	}
 	if (boxX2 < 640) {
 		if (InputX > 1)
 		{
-			boxX += 10;
-			boxX2 += 10;
+			boxX += 5;
+			boxX2 += 5;
 		}
 	}
 	return this;
