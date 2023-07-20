5a6a1947ec604ac648131f606b9a7f8e19008e15
Kina0303 <122337142+Kina0303@users.noreply.github.com>
2023-07-20 13:17:05 +0900

    判定

--- bf2-3b/Player.cpp
+++ bf2-3b/Player.cpp
@@ -8,7 +8,7 @@ int Gvy = 0;
 Player::Player()
 {
 	playerX = 100;
-	playerY = 
+	playerY = 340;
 	vx = 0.5;
 	vy = 0.5;
 	e = 0.8;
@@ -27,8 +27,8 @@ AbstractScene* Player::Update()
 		{
 			Gvy = 3;
 			// 押されている
-			boxY -= Gvy;
-			boxY2 -= Gvy;
+			playerY -= Gvy;
+			/*boxY2 -= Gvy;*/
 		}
 	}
 	else if ((PAD_INPUT::OnButton(XINPUT_BUTTON_A)))
@@ -37,65 +37,68 @@ AbstractScene* Player::Update()
 		{
 			Gvy = 20;
 			// 押されている
-			boxY -= Gvy;
-			boxY2 -= Gvy;
+			playerY -= Gvy;
+			/*boxY2 -= Gvy;*/
 		}
 	}
 
 	
 	//地面に立っていないとき
 	//右地面
-	if ((S1_Landright_X <= boxX2 || S1_Landleft_Width >= boxX)&& S1_Landright_Y == boxY2) {
+	if ((S1_Landright_X-64 <= playerX && S1_Landright_Width >= playerX)&& S1_Landright_Y == playerY+64) {
 		Gvy = 0;
 	}
 	// 右地面左側面
-	else if (S1_Landright_X <= boxX2 && S1_Landright_Y < boxY2) {
+	else if (S1_Landright_X <= playerX && S1_Landright_Y < playerY+64) {
 		/*vx -= vx*e;*/
 		Gvy = 1;
 		boxX -= vx;
 		boxX2 -= vx;
-		boxY += Gvy;
+		playerY += Gvy;
 		boxY2 += Gvy;
 	}
 	//左地面
-	else if (S1_Landleft_X <= boxX2 && S1_Landleft_Width >= boxX && S1_Landleft_Y == boxY2) {
+	else if ((S1_Landleft_X <= playerX+64 && S1_Landleft_Width >= playerX+20) && S1_Landleft_Y == playerY+64) {
 		Gvy = 0;
 	}
 	// 左地面右側面
-	else if (S1_Landleft_Width >= boxX && S1_Landleft_Y < boxY2) {
+	else if (S1_Landleft_Width >= playerX+20 && S1_Landleft_Y < playerY+64) {
 		Gvy = 1;
-		boxX = 160;
-		boxX2 = 210;
-		boxY += Gvy;
-		boxY2 += Gvy;
+		playerX = 140;
+		/*boxX2 = 210;*/
+		playerY += Gvy;
+		/*boxY2 += Gvy;*/
 		
 	}
 	//空中床
-	else if (S1_Flooting_X <= boxX2 && S1_Flooting_Width >= boxX && S1_Flooting_Y == boxY2) {
+	else if ((S1_Flooting_X <= playerX && S1_Flooting_Width >= playerX+64) && S1_Flooting_Y == playerY+64) {
 		Gvy = 0;
+	}
+	else if (S1_Flooting_X <= playerX && S1_Flooting_Width >= playerX + 64) {
+
 	}
 	else
 	{
 		// ジャンプが押されていない
 		Gvy = 1;
-		boxY += Gvy;
-		boxY2 += Gvy;
+		/*playerX += Gvy;*/
+		playerY += Gvy;
 	}
 
 
 	GetJoypadAnalogInput(&InputX, &InputY, DX_INPUT_PAD1);
-	if (boxX > 0) {
+	if (playerX+64 > 0) {
 		if (InputX < -1)
 		{
-			boxX -= 6;
-			boxX2 -= 6;
+			playerX -= 6;
+			/*playerY -= 6;*/
 		}
 	}
-	if (boxX2 < 640) {
+	if (playerX < 640) {
 		if (InputX > 1)
 		{
-			boxX += 6;
-			boxX2 += 6;
+			playerX += 6;
+			/*playerY += 6;*/
 		}
 	}
 	return this;
@@ -106,6 +109,6 @@ void Player::Draw() const
 	DrawFormatString(0, 0, 0xffffff,"%d",InputX, TRUE);
 	
 	/*DrawBox(boxX, boxY,boxX2, boxY2+5, 0xffffff, TRUE);*/
-	DrawBox(boxX, boxY, boxX2, boxY2 , 0xff2255, FALSE);
-	DrawGraph(boxX-5, boxY -10, Playerimg[0], TRUE);
+	/*DrawBox(boxX, boxY, boxX2, boxY2 , 0xff2255, FALSE);*/
+	DrawGraph(playerX, playerY, Playerimg[0], TRUE);
 }
